5485b9b6259d49f4f836fde8b9dabecc65b964fe
juliorosero <david-kamina@hotmail.com>
2017-02-15 09:16:42 -0500

    Add files via upload

---
+++ PSK.m
@@ -0,0 +1,67 @@
+function PSK(g,f)
+ 
+%Modulación PSK
+%Ejemplo para ingresar datos:
+%PSK([1 0 1 1 1 0 0 0 1 1],2)
+ 
+if nargin > 2
+ 
+error('Demasiados argumento de entrada');
+ 
+elseif nargin==1
+ 
+f=1;
+ 
+end
+ 
+if f<1;
+ 
+error('Frecuencia tiene que se mayor que 1');
+ 
+end
+ 
+t=0:2*pi/99:2*pi;
+ 
+cp=[];sp=[];
+ 
+mod=[];mod1=[];bit=[];
+ 
+for n=1:length(g);
+ 
+if g(n)==0;
+ 
+die=-ones(1,100);%Modulante
+ 
+se=zeros(1,100);%Señal
+ 
+else g(n)==1;
+ 
+die=ones(1,100);%Modulante
+ 
+se=ones(1,100);%Señal
+ 
+end
+ 
+c=sin(f*t);
+ 
+cp=[cp die];
+ 
+mod=[mod c];
+ 
+bit=[bit se];
+ 
+end
+ 
+bpsk=cp.*mod;
+ 
+subplot(2,1,1);plot(bit,'LineWidth',1.5);grid on;
+ 
+title('Señal Binaria');
+ 
+axis([0 100*length(g) -2.5 2.5]);
+ 
+subplot(2,1,2);plot(bpsk,'LineWidth',1.5);grid on;
+ 
+title('Modulación PSK');
+ 
+axis([0 100*length(g) -2.5 2.5]);
